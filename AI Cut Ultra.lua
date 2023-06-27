094783a12bf02b02343b86e9068a1b2396f4af61
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-06-27 19:32:12 +0200

    next

---
+++ AI Cut Ultra.lua
@@ -0,0 +1,28 @@
+--- === com.pravdomil.ai-cut-ultra ===
+
+local fcp = require "cp.apple.finalcutpro"
+
+local plugin = {
+    id = "com.pravdomil.ai-cut-ultra",
+    group = "finalcutpro",
+    dependencies = {
+        ["finalcutpro.commands"] = "fcpxCmds",
+    }
+}
+
+function plugin.init(deps)
+    if not fcp:isSupported() then
+        return
+    end
+
+    deps.fcpxCmds
+        :add("AI Cut Ultra")
+        :whenActivated(run)
+end
+
+function run()
+    fcp:launch()
+    hs.alert.show("AI Cut Ultra here!")
+end
+
+return plugin
