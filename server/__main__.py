2091f16d36a24550053e071b74c6bebd968c0f55
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-06-28 17:25:29 +0200

    next

--- server/__main__.py
+++ server/__main__.py
@@ -1,1 +1,59 @@
-print("Hello!")
+import sys
+from dataclasses import dataclass
+
+# noinspection PyUnresolvedReferences
+from google.colab import drive
+
+
+# State
+
+@dataclass
+class Initializing:
+    _: None
+
+
+@dataclass
+class MountingDrive:
+    _: None
+
+
+@dataclass
+class Ready:
+    _: None
+
+
+@dataclass
+class Exiting:
+    _: None
+
+
+# Functions
+
+def __main__():
+    try:
+        state_step(Initializing(None))
+    except KeyboardInterrupt:
+        state_step(Exiting(None))
+
+
+def state_step(a):
+    if isinstance(a, Initializing):
+        state_step(MountingDrive(None))
+
+    elif isinstance(a, MountingDrive):
+        print("Mounting Drive...")
+        drive.mount('/content/drive')
+        state_step(Ready(None))
+
+    elif isinstance(a, Ready):
+        print("Ready")
+
+    elif isinstance(a, Exiting):
+        print("Quiting...")
+        sys.exit()
+
+    else:
+        raise ValueError("Unknown variant.")
+
+
+__main__()
