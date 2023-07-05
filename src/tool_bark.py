c994af8dcd6b40cd47cd06c1e9edf9d075238e19
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-05 22:29:24 +0200

    next

--- src/tool_bark.py
+++ src/tool_bark.py
@@ -1,5 +1,11 @@
 import task
+# noinspection PyPackageRequirements
+import bark
+import scipy
 
 
 def main(a: task.Bark):
-    print("Bark!")
+    bark.preload_models()
+    audio = bark.generate_audio(a.prompt)
+    scipy.io.wavfile.write(a.output_filename, bark.SAMPLE_RATE, audio)
+
