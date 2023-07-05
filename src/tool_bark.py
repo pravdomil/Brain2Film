3809176013bad8e88bb827e67dbd7d4bceb47866
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-05 22:47:05 +0200

    next

--- src/tool_bark.py
+++ src/tool_bark.py
@@ -12,4 +12,10 @@ def main(a: task.Bark):
     print("Bark: \"" + a.prompt.replace("\n", ", ") + "\"")
 
     audio = bark.generate_audio(a.prompt)
-    scipy.io.wavfile.write(os.path.join(config.output_dir, a.output_filename), bark.SAMPLE_RATE, audio)
+
+    if a.output_filename.endswith(".wav"):
+        output_filename = a.output_filename
+    else:
+        output_filename = a.output_filename + ".wav"
+
+    scipy.io.wavfile.write(os.path.join(config.output_dir, output_filename), bark.SAMPLE_RATE, audio)
