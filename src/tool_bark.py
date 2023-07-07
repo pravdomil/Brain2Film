67f8a420eb7d73549e3148d49235e0c9fdccc7aa
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-07 21:07:43 +0200

    next

--- src/tool_bark.py
+++ src/tool_bark.py
@@ -2,6 +2,7 @@ import os
 
 # noinspection PyPackageRequirements
 import bark
+import numpy
 import pydub
 
 import config
@@ -12,11 +13,12 @@ def main(a: task.Bark):
     print("Bark: \"" + a.prompt.replace("\n", "\\n") + "\"")
 
     audio = bark.generate_audio(a.prompt, history_prompt="v2/en_speaker_1")
+    normalized = numpy.int16(audio * 2 ** 15)
 
     if a.output_filename.endswith(".mp3"):
         output_filename = a.output_filename
     else:
         output_filename = a.output_filename + ".mp3"
 
-    segment = pydub.AudioSegment(audio.tobytes(), sample_width=2, frame_rate=bark.SAMPLE_RATE, channels=1)
+    segment = pydub.AudioSegment(normalized.tobytes(), sample_width=2, frame_rate=bark.SAMPLE_RATE, channels=1)
     segment.export(os.path.join(config.output_dir, output_filename), format="mp3", bitrate="320k")
