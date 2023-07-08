294d5b495729331dae4b2a6cd2128d5042a0df93
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-08 23:51:17 +0200

    next

--- src/commandpost.py
+++ src/commandpost.py
@@ -182,7 +182,7 @@ def parse_task(
         return task.Task(type_)
 
     else:
-        raise Exception("Notes must start with pix or bark or voice or ldm or craft.")
+        raise Exception("Notes must start with pix or up or bark or voice or ldm or craft.")
 
 
 def parse_time(a: str) -> tuple[int, int]:
