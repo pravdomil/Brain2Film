810f98f0e9f2ec3b3515ab048ded3436ae5bb536
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-08 23:18:27 +0200

    next

---
+++ src/tool_resrgan.py
@@ -0,0 +1,14 @@
+import basicsr.archs.rrdbnet_arch
+import realesrgan
+
+import config
+
+
+def main():
+    upsampler = realesrgan.RealESRGANer(
+        model_path="https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth",
+        model=basicsr.archs.rrdbnet_arch.RRDBNet(
+            num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4),
+        scale=4,
+        device=config.device,
+    )
