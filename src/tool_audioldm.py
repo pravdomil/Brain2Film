f0b7a961ed29e2615333e46ad242e9ed3c08c34b
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-08 15:54:55 +0200

    next

--- src/tool_audioldm.py
+++ src/tool_audioldm.py
@@ -11,12 +11,12 @@ import utils
 def main(a: task.AudioLDM):
     print("AudioLDM: \"" + a.prompt.replace("\n", "\\n") + "\"")
 
+    torch.manual_seed(config.seed)
     pipe = diffusers.AudioLDMPipeline.from_pretrained(
         "cvssp/audioldm-s-full-v2",
         torch_dtype=torch.float16
     ).to(config.device)
 
-    torch.manual_seed(config.seed)
     audio = pipe(a.prompt, num_inference_steps=10, audio_length_in_s=a.duration).audios[0]
 
     utils.save_to_mp3(audio, os.path.join(config.output_dir, a.output_filename))
