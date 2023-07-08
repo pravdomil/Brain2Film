9144d509fa58b6f7f7dac9c8241eb1bfec7b7491
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-08 17:34:05 +0200

    next

--- src/tool_audioldm.py
+++ src/tool_audioldm.py
@@ -22,8 +22,8 @@ def main(a: task.AudioLDM):
     ).to(config.device)
 
     audio = pipe(a.prompt, num_inference_steps=10, audio_length_in_s=a.duration).audios[0]
-    audio_44khz = librosa.resample(audio, orig_sr=16000, target_sr=sample_rate)
-    audio_enhanced = enhance_audio(audio_44khz)
+    audio_resampled = librosa.resample(audio, orig_sr=16000, target_sr=sample_rate)
+    audio_enhanced = enhance_audio(audio_resampled)
 
     utils.save_to_mp3(audio_enhanced, os.path.join(config.output_dir, a.output_filename))
 
