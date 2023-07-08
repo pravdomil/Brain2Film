1f6e26163ac9a9939e4a0ac1f00fc5d1535d3cb6
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-08 16:22:20 +0200

    next

--- src/tool_audioldm.py
+++ src/tool_audioldm.py
@@ -1,11 +1,16 @@
 import os
 
 import diffusers
+import librosa
+import numpy
+import scipy.signal
 import torch
 
 import config
 import task
 import utils
+
+sample_rate = 44100
 
 
 def main(a: task.AudioLDM):
@@ -18,5 +23,14 @@ def main(a: task.AudioLDM):
     ).to(config.device)
 
     audio = pipe(a.prompt, num_inference_steps=10, audio_length_in_s=a.duration).audios[0]
+    audio_44khz = librosa.resample(audio, orig_sr=16000, target_sr=sample_rate)
+    audio_highpass = apply_highpass_mix(audio_44khz)
 
-    utils.save_to_mp3(audio, os.path.join(config.output_dir, a.output_filename))
+    utils.save_to_mp3(audio_highpass, os.path.join(config.output_dir, a.output_filename))
+
+
+def apply_highpass_mix(input: numpy.ndarray, highpass_vol=0.2, mix_vol=0.8, cutoff=11000, order=6):
+    octave_up = librosa.effects.pitch_shift(input, sr=sample_rate, n_steps=12, res_type="soxr_vhq") * highpass_vol
+    b, a = scipy.signal.butter(order, cutoff, fs=sample_rate, btype="highpass", analog=False)
+    highpass = scipy.signal.lfilter(b, a, octave_up)
+    return input * mix_vol + highpass
