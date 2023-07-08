4b22aa0e060d1b89bc4dcff4bd6d0ec3ccffba71
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-08 16:34:52 +0200

    next

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,15 @@
-import bark
 import numpy
 import pydub
 
 
-def save_to_mp3(audio, filename: str) -> str:
+def save_to_mp3(audio: numpy.ndarray, filename: str, sample_rate=44100) -> str:
     if filename.endswith(".mp3"):
         filename = filename
     else:
         filename = filename + ".mp3"
 
     normalized = numpy.int16(audio * 2 ** 15)
-    segment = pydub.AudioSegment(normalized.tobytes(), sample_width=2, frame_rate=bark.SAMPLE_RATE, channels=1)
+    segment = pydub.AudioSegment(normalized.tobytes(), sample_width=2, frame_rate=sample_rate, channels=1)
     segment.export(filename, format="mp3", bitrate="320k")
 
     return filename
