bed97444ab5e1f97dcc5d41d10d635e4d1a0fc36
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-07-17 16:10:24 +0200

    next

--- src/utils.py
+++ src/utils.py
@@ -2,14 +2,7 @@ import numpy
 import pydub
 
 
-def save_to_mp3(audio: numpy.ndarray, filename: str, sample_rate, title: str) -> str:
-    if filename.endswith(".mp3"):
-        filename = filename
-    else:
-        filename = filename + ".mp3"
-
+def save_to_mp3(audio: numpy.ndarray, filename: str, sample_rate, title: str):
     normalized = numpy.int16(audio * 2 ** 15)
     segment = pydub.AudioSegment(normalized.tobytes(), sample_width=2, frame_rate=sample_rate, channels=1)
     segment.export(filename, format="mp3", bitrate="320k", tags={"title": title})
-
-    return filename
